5dd0deef881af03a4ecb47df20de5373d24ede93
Gennadi Egorov <gennadi_egorov@epam.com>
2024-03-21 23:41:45 +0100

    add some options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,14 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, for help with jumping.
---  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 vim.g.have_nerd_font = true
 
 vim.opt.hlsearch = true
+
+-- undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
