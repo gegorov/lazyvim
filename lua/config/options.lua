ce0e3b9dec28481ee7b2e68fed72ba52bf8145bd
Gennadi Egorov <gennadi_egorov@epam.com>
2024-03-21 13:28:17 +0100

    add basic configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, for help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+
+vim.g.have_nerd_font = true
+
+vim.opt.hlsearch = true
