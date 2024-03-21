d383d69a293516495447367058d7e6fe6076b593
Gennadi Egorov <gennadi_egorov@epam.com>
2024-03-21 23:42:45 +0100

    add conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,24 @@
+return {
+  "stevearc/conform.nvim",
+  optional = true,
+  opts = {
+    formatters_by_ft = {
+      ["javascript"] = { "biome" },
+      ["javascriptreact"] = { "biome" },
+      ["typescript"] = { "biome" },
+      ["typescriptreact"] = { "biome" },
+      ["vue"] = { "biome" },
+      ["css"] = { "biome" },
+      ["scss"] = { "biome" },
+      ["less"] = { "biome" },
+      ["html"] = { "biome" },
+      ["json"] = { "biome" },
+      ["jsonc"] = { "biome" },
+      ["yaml"] = { "biome" },
+      ["markdown"] = { "biome" },
+      ["markdown.mdx"] = { "biome" },
+      ["graphql"] = { "biome" },
+      ["handlebars"] = { "biome" },
+    },
+  },
+}
